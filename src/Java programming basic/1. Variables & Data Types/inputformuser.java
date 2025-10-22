5871bac457dff338a8fb6fa82ef2eae35b9af493
coder21-space <kaushal21@gmail.com>
2025-10-22 22:43:27 +0530

    create folder structure and basic java

---
+++ src/Java programming basic/1. Variables & Data Types/inputformuser.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class inputformuser {
+    public static void main(String arge[]) {
+        Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+        // System.out.println(age);
+       
+        //    String name = sc.nextLine();
+        // System.out.println(name);
+
+        //        String course = sc.next();
+        // System.out.println(course);
+
+        // sum of 2 number by user input
+        int a =sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b ;
+        System.out.println(sum);
+    }
+
+}
---
+++ src/Java programming basic/1. Variables & Data Types/javabasic.java
@@ -0,0 +1,32 @@
+public class javabasic {
+    public static void main(String arge[]) {
+        System.out.println("hello java");
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        float pi = 3.14f;
+        System.out.println(pi);
+
+        boolean isjavafun = false;
+        System.out.println(isjavafun);
+
+        String name = "kaushal";
+        System.out.println(name);
+
+        a = 100;
+        b = 100;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
